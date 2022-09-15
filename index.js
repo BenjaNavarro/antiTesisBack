20bafaf0a7bc7030601da5ab027af7a5afce6705
Benjamín Navarro <benjamin.navarro@globalhap.com>
2022-09-15 01:13:13 -0300

    socket

--- index.js
+++ index.js
@@ -1,13 +1,36 @@
-const express = require('express')();
+const app = require('express')();
+const server = require('http').createServer(app);
+const cors = require('cors');
+
+const io = require('socket.io')(server,{
+  cors: {
+    origin:'*',
+    methods: ['GET','POST','PUT','DELETE','OPTIONS','HEAD']
+  }
+});
+
+app.use(cors());
+
 const PORT = 8000;
-const app = express();
-
-app.use(express.json());
-
-app.listen(
-  PORT,()=>{console.log('Server on Port '+PORT)}
-);
 
 app.get('/',(req,res)=>{
-  return res.json('It is Alive!').status(200)
+  res.send('It is alive!');
 });
+
+io.on('connection',(socket)=>{
+  socket.emit('me',socket.id);
+
+  socket.on('disconnect',()=>{
+    socket.broadcast.emit('callended');
+  });
+
+  socket.on('calluser',({userToCall,signalData,from,name})=>{
+    io.to(userToCall).emit('callUser',{signal:signalData,from,name});
+  });
+
+  socket.on('answercall',(data)=>{
+    io.to(data.to).emit('callaccepted',data.signal);
+  });
+})
+
+server.listen(PORT,()=> console.log('Server on port: ',PORT));
