8864d0bfee1ccf20efec7eb7adedbf3de24d813e
Benjamín Navarro <benjamin.navarro@globalhap.com>
2022-09-22 00:11:57 -0300

    cambios socket

--- index.js
+++ index.js
@@ -18,17 +18,21 @@ app.get('/',(req,res)=>{
 });
 
 io.on('connection',(socket)=>{
+  console.log('Socket Connected');
   socket.emit('me',socket.id);
 
   socket.on('disconnect',()=>{
+    console.log('Socket Disconnected');
     socket.broadcast.emit('callended');
   });
 
   socket.on('calluser',({userToCall,signalData,from,name})=>{
-    io.to(userToCall).emit('callUser',{signal:signalData,from,name});
+    console.log('Calling User');
+    io.to(userToCall).emit('calluser',{signal:signalData,from,name});
   });
 
   socket.on('answercall',(data)=>{
+    console.log('Calling User');
     io.to(data.to).emit('callaccepted',data.signal);
   });
 })
